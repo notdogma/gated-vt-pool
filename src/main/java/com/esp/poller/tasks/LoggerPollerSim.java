fdbab8d5d54cba45e23744516fcfc9c3690b3044
notdogma <notdogma@outlook.com>
2025-09-14 19:39:50 -0400

    fixed err handler

--- src/main/java/com/esp/poller/tasks/LoggerPollerSim.java
+++ src/main/java/com/esp/poller/tasks/LoggerPollerSim.java
@@ -11,7 +11,6 @@ import com.esp.poller.ruleCache.RuleCacheSim;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
